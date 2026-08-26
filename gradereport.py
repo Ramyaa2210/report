1acee5928907802198f1e0def2da25bcd056d944
Ramyaa2210 <garla.balaji2024@vitstudent.ac.in>
2026-08-26 15:18:29 +0530

    add report program

---
+++ gradereport.py
@@ -0,0 +1,15 @@
+def calculate_grade(marks):
+    if marks >= 90:
+        return "A"
+    elif marks >= 80:
+        return "B"
+    elif marks >= 70:
+        return "C"
+    elif marks >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+marks = int(input("Enter marks: "))
+print("Grade:", calculate_grade(marks))
